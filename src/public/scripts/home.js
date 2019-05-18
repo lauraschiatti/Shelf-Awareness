e889877a236909c3a541e303860f5c2fd3da33c4
jefimija <zivkovic.jefimijaa@gmail.com>
2019-05-18 23:50:52 +0200

    login frontend connected with backend

---
+++ src/public/scripts/home.js
@@ -0,0 +1,28 @@
+$(document).ready ( function(){
+  fetch('v2/users/loggedInUser')
+  .then(function(response){
+      response.json()
+      .then(function(data) {
+        var user = data;
+        if(user!=undefined){
+          $('#login').empty();
+          $('#loggedIn').empty();
+          $('#loggedIn').append('<a class="btn btn-default btn-sm ml-3" id="loggedIn">'+user.name+'</a>');
+          $('#login').append('<a class="btn btn-default btn-sm ml-3" id="signOut"><i class="fa fa-sign-in"></i> Sign out</a>');
+        }
+      });
+    });
+});
+
+$(document).on("click","#signOut", function(){
+  fetch('v2/users/logoutUser')
+  .then(function(response){
+      response.json()
+      .then(function(data) {
+        var user = data;
+        if(user==undefined){
+          window.location="/index.html";
+        }
+      });
+    });
+});

b75811a4ba76153fc1f1c4eb113f276b3461e485
Ana Pesko <anap1003@gmail.com>
2019-05-31 16:26:53 +0200

    added links to events; delete icons

--- src/public/scripts/events.js
+++ src/public/scripts/events.js
@@ -17,6 +17,14 @@ $(document).ready(function() {
             // TODO: change column held_on to timestamp? or check if the time of the event can be explicitly stated this way
             listItem.innerHTML =
                 `<div class="card mb-4">
+                    <div class="card-footer">
+                        <div class="row text-center">
+                            <div class="col-xs-6">
+                               <i class="fa fa-map-marker"></i>
+                               <small class="text-dark">${location}</small>
+                            </div>
+                        </div>
+                    </div>
                     <div style = "width: 50em; height: 20em; " >
                       <a class="text-dark" href="pages/event-page.html?id=${id}">
                           <img class="card-header" src="${book.cover}" alt="Card image cap" style="max-width: 100%;max-height: 100%;" >
@@ -26,15 +34,6 @@ $(document).ready(function() {
                          <small class="text-primary" > ${date}</small>
                          <p class="card-title"><a class="text-dark" href="pages/show-event.html?id=${id}">${book.title}</a></p>
                          <p class="card-subtitle"><small class="font-italic">${author.name}</small></p>
-
-                         <div class="card-footer">
-                             <div class="row text-center">
-                                 <div class="col-xs-6">
-                                    <img src="assets/open-iconic/svg/location.svg" alt="Location">
-                                    <small class="text-dark">${location}</small>
-                                 </div>
-                             </div>
-                         </div>
                     </div>
                 </div>`;
             $("#events-list").append(listItem);

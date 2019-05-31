73065896682258e8266c6ec5697ec1f5ac43f6fc
jefimija <zivkovic.jefimijaa@gmail.com>
2019-05-31 19:18:31 +0200

    swagger refactor; book issues fixed

--- src/public/scripts/book.js
+++ src/public/scripts/book.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
-    $.urlParam = function(name){
-        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-        return results[1] || 0;
-    }
+  $.urlParam = function(name) {
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    return results[1] || 0;
+  }
 
-    var id = $.urlParam('id');
+  var id = $.urlParam('id');
 
-    fetch('v2/books/' + id).then(function(response) {
-        return response.json();
-    }).then(function(json) {
-        let { title, cover, abstract, author, genre, price, status, interview } = json;
+  fetch('v2/books/' + id).then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    let {
+      title,
+      cover,
+      abstract,
+      author,
+      genre,
+      price,
+      status,
+      interview
+    } = json;
 
-        $("#book").append(`<div class="row justify-content-md-center">
+    $("#book").append(`<div class="row justify-content-md-center">
             <div class="card m-5 col-xs-11 col-md-8">
                 <div class="card-body p-5">
                     <div class="row pb-4">
@@ -58,13 +67,80 @@ $(document).ready(function() {
 
                     <hr>
 
-                    <small class="font-weight-light">
-                        <a href="#" class="font-italic"><i class="fa fa-pencil-square-o"></i> Review this product</a>
-                        <br>
-                        Share your thoughts with other customers
+                    <small class="font-weight-light" id="bookReviewButton" >
+                      <a nohref onClick="reviewCreate();" style = "text-decoration: underline; color: #D10024"  class="font-italic"><i class="fa fa-pencil-square-o"></i> Review this product</a>
+                      <br>
+                      Share your thoughts with other customers
                     </small>
                 </div>
             </div>
         </div>`);
-    });
+  });
 });
+
+function reviewCreate() {
+  $('#bookReviewButton').empty();
+  $('#reviewsDiv').empty();
+  $('#controls').empty();
+  $('#customerReviews').empty();
+  $('#customerReviews').append(`<h3 class="text-danger h3-header" style = "text-align: left; margin-left: 175px;">What did you think about this book?</h3>`);
+  $('#reviewsDiv').append(`
+        <form id="reviewForm" class="form-review">
+          <div class="input-group input-group-lg mb-3">
+            <textarea rows="4" cols="50" id="inputReview" class="form-control" placeholder="Enter review here" required ></textarea>
+          </div>
+          <br>
+          <button class="btn btn-lg btn-primary btn-block" style = "width: 50%;" id="reviewSubmit" type="button">Submit</button>
+        </form>`);
+  return false;
+};
+
+$(document).on("click", "#reviewSubmit", function(e) {
+  e.preventDefault();
+  var comment = document.getElementById("inputReview").value;
+  var user = getLoggedInUser();
+  $.urlParam = function(name) {
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    return results[1] || 0;
+  }
+
+  var id = $.urlParam('id');
+  var ok = true;
+  if (comment == "") {
+    ok = false;
+  }
+  if (ok) {
+    $.ajax({
+      url: "v2/reviews/create",
+      contentType: "application/json",
+      dataType: "text",
+      type: "POST",
+      data: JSON.stringify({
+        "userId": user.id,
+        "comment": comment,
+        "bookId": id
+      }),
+      success: function(data) {
+        var message = data;
+        if (message == "OK") {
+          $('#bookReviewButton').append(`
+                    <a nohref onClick="reviewCreate();" style = "text-decoration: underline; color: #D10024"  class="font-italic"><i class="fa fa-pencil-square-o"></i> Review this product</a>
+                    <br>
+                    Share your thoughts with other customers`);
+
+          loadReviews(id);
+        }
+      }
+
+    });
+  }
+});
+// function getLoggedInUser() {
+//   fetch('v2/users/loggedInUser')
+//     .then(function(response) {
+//       response.json()
+//         .then(function(data) {
+//           return data;
+//         });
+//     });
+// }

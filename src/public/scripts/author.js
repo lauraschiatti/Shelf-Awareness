d548c2bf2936c4a870b0e0fb0baca2355e50fb11
lauricdd <lauri_cdd@hotmail.com>
2019-06-01 23:20:39 +0200

    author page

--- src/public/scripts/author.js
+++ src/public/scripts/author.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
-  // getUser();
-  // getAuthor();
-  var authorId = window.location.href.split("=")[1];
-  fetch('v2/authors/' + authorId).then(function(response) {
+  $.urlParam = function(name){
+      var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+      return results[1] || 0;
+  }
+  var id = $.urlParam('id');
+
+  fetch('v2/authors/' + id).then(function(response) {
     return response.json();
   }).then(function(json) {
     let {
@@ -10,86 +13,34 @@ $(document).ready(function() {
       bio,
       picture
     } = json[0];
-    $("#author").append(`<div class="row justify-content-md-center">
-            <div class="card m-5 col-xs-11 col-md-8">
+    $("#author").append(
+        `<div class="col-xs-10 col-md-10 col-lg-8 offset-lg-2 main mt-5">
+            <div class="row">          
                 <div class="card-body p-5">
+                      
                     <div class="row pb-4">
                         <div class="col-sm-8 border-right">
-                            <h4 class="text-dark mb-0">${name}</h4>
+                            <h4 class="text-primary mb-0">${name}</h4>
                         </div>
                         <div class="col-sm-4">
                             <img src="${picture}" class="rounded mx-auto d-block img-fluid" style="width:70%;" />
                         </div>
                     </div>
-
-                    <dl >
-                        <dt>Biography</dt>
-                        <dd class="text-justify"><small class="font-weight-light">${bio}</small></dd>
-                    </dl>
+        
+             
+                       <h5><strong>Biography</strong></h5>
+                       <p class="text-justify font-weight-light">${bio}</p>
+ 
+                    
+                    <a class="text-primary" href="pages/books_by_author.html?id=${id}"> <i class="fa fa-book"></i> Books by ${name}</a>
+                    
+                    <br>
+                    
+                    <a href="pages/all_authors.html"> <i class="fa fa-angle-double-left mt-5 justify-content-end"></i> Back to all authors </a>
+                  </div>       
+                  
                 </div>
             </div>
         </div>`);
   });
 });
-
-// function getUser(){
-//   fetch('v2/users/loggedInUser')
-//     .then(function(response) {
-//       response.json()
-//         .then(function(data) {
-//           var user = data;
-//           if (user != undefined) {
-//             $('#login').empty();
-//             $('#loggedIn').empty();
-//             $('#loggedIn').append('<a class="btn btn-default btn-sm ml-3" id="loggedIn">' + user.name + '</a>');
-//             $('#login').append('<a class="btn btn-default btn-sm ml-3" id="signOut"><i class="fa fa-sign-in"></i> Sign out</a>');
-//           }
-//         });
-//     });
-// }
-
-// function getAuthor() {
-//   var authorId = window.location.href.split("=")[1];
-//   fetch('v2/books/' + authorId).then(function(response) {
-//     return response.json();
-//   }).then(function(json) {
-//     let {
-//       name,
-//       bio,
-//       picture
-//     } = json;
-//     $("#book").append(`<div class="row justify-content-md-center">
-//             <div class="card m-5 col-xs-11 col-md-8">
-//                 <div class="card-body p-5">
-//                     <div class="row pb-4">
-//                         <div class="col-sm-8 border-right">
-//                             <h4 class="text-dark mb-0">${name}</h4>
-//                         </div>
-//                         <div class="col-sm-4">
-//                             <img src="${picture}" class="rounded mx-auto d-block img-fluid" style="width:70%;">
-//                         </div>
-//                     </div>
-//
-//                     <dl >
-//                         <dt>Biography</dt>
-//                         <dd class="text-justify"><small class="font-weight-light">${bio}</small></dd>
-//                     </dl>
-//                 </div>
-//             </div>
-//         </div>`);
-//   });
-// }
-//
-// $(document).on("click", "#signOut", function(e) {
-//   e.preventDefault();
-//   fetch('v2/users/logoutUser')
-//     .then(function(response) {
-//       response.json()
-//         .then(function(data) {
-//           var user = data;
-//           if (user.loggedin == false) {
-//             window.location = "/index.html";
-//           }
-//         });
-//     });
-// });

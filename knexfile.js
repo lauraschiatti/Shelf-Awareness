3fa1ae4d03d483f57fa11acc30f4d35a637ddacb
lauricdd <lauri_cdd@hotmail.com>
2019-06-21 12:16:11 +0200

    connection password fixed

--- knexfile.js
+++ knexfile.js
@@ -24,7 +24,7 @@ module.exports = {
         host : 'ec2-79-125-4-72.eu-west-1.compute.amazonaws.com',
         port: '5432',
         user : 'mvqabcepsvczaw',
-        password : '97bcdb34750cbb28d05cf00c505db2c64fb09cc8ae97d1117b3bf63986477f23\n',
+        password : '97bcdb34750cbb28d05cf00c505db2c64fb09cc8ae97d1117b3bf63986477f23',
         database : 'dbt6i2gv9m7jn'
     },
     migrations: {

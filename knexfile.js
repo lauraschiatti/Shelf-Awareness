296d6c7c910ce780452fa7c6b44f0ebcccca29c1
lauricdd <lauri_cdd@hotmail.com>
2019-06-26 13:24:41 +0200

    active landmarks by page fixed. Final knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,31 +1,29 @@
-// Update with your config settings.
-
 module.exports = {
 
-  development: {
-    client: 'pg',
-    connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'root',
-      database : 'shelfawareness'
+    development: {
+        client: 'pg',
+        connection: {
+            host : '127.0.0.1',
+            user : 'postgres',
+            password : 'root',
+            database : 'shelfawareness'
+        },
+        migrations: {
+            directory: __dirname + '/knex/migrations',
+        },
+        seeds: {
+            directory: __dirname + '/knex/seeds'
+        }
     },
-    migrations: {
-        directory: __dirname + '/knex/migrations',
-    },
-    seeds: {
-        directory: __dirname + '/knex/seeds'
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        migrations: {
+            directory: __dirname + '/knex/migrations',
+        },
+        seeds: {
+            directory: __dirname + '/knex/seeds'
+        }
     }
-  },
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-        directory: __dirname + '/knex/migrations',
-    },
-    seeds: {
-        directory: __dirname + '/knex/seeds'
-    }
-  }
 
 };
